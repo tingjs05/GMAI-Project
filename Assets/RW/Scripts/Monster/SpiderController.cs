cb117a4388d8e6122d762d4971b6672c331c3686
Samuel Ting <tingjs05@gmail.com>
2024-06-12 18:31:51 +0800

    setup spider stuff

--- Assets/RW/Scripts/Monster/SpiderController.cs
+++ Assets/RW/Scripts/Monster/SpiderController.cs
@@ -6,10 +6,26 @@ using Astar.Pathfinding;
 [RequireComponent(typeof(SpiderData), typeof(Agent))]
 public class SpiderController : MonoBehaviour
 {
+    // componenets
+    public SpiderData data { get; private set;}
+    public Agent agent { get; private set; }
+    public Animator anim { get; private set; }
+
+    // children objects
+    GameObject hitbox;
+    GameObject parryIndicator;
+
     // Start is called before the first frame update
     void Start()
     {
+        // get components
+        data = GetComponent<SpiderData>();
+        agent = GetComponent<Agent>();
+        anim = GetComponentInChildren<Animator>();
         
+        // get children objects
+        hitbox = transform.GetChild(1).gameObject;
+        parryIndicator = transform.GetChild(2).gameObject;
     }
 
     // Update is called once per frame
--- Assets/RW/Scripts/Monster/SpiderData.cs
+++ Assets/RW/Scripts/Monster/SpiderData.cs
@@ -10,6 +10,20 @@ public class SpiderData : MonoBehaviour
     [SerializeField] float maxHealth = 15f;
     [SerializeField] float walkSpeed = 7f;
     [SerializeField] float runSpeed = 12f;
+    [SerializeField] float damage = 5f;
+    [SerializeField] float strongDamage = 15f;
+
+    [Header("Ranges")]
+    [SerializeField] float patrolRange = 8f;
+    [SerializeField] float detectionRange = 3f;
+
+    [Header("Durations")]
+    [SerializeField] float stunDuration = 5f;
+
+
+    [Header("Others")]
+    [SerializeField] LayerMask playerMask;
+    [SerializeField] bool showGizmos = true;
 
     #endregion
 
@@ -19,7 +33,23 @@ public class SpiderData : MonoBehaviour
 
     #region Properties
 
+    // stats
     public float MaxHealth => maxHealth;
+    public float WalkSpeed => walkSpeed;
+    public float RunSpeed => runSpeed;
+    public float Damage => damage;
+    public float StrongDamage => strongDamage;
+
+    // ranges
+    public float PatrolRange => patrolRange;
+    public float DetectionRange => detectionRange;
+
+    // durations
+    public float StunDuration => stunDuration;
+
+    // others
+    public LayerMask PlayerMask => playerMask;
+    public bool ShowGizmos => showGizmos;
 
     #endregion
 }
